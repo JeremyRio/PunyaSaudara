4795be1aac60ae7932d24855d0433696fef9b8d9
Radenz <radenrifqirahman@gmail.com>
2022-11-22 02:26:25 +0700

    feat: display error messages

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -18,11 +18,14 @@ class Asset(http.Controller):
 
     @http.route('/asset/items/new', auth='user', website=True, methods=["POST"])
     def items_new_handle(self, **kw):
-        params = http.request.get_http_params()
-        http.request.env["aset.barang"].create(
-            {"name": params["name"], "qty": 0})
-        http.request.cr.commit()
-        return http.request.make_response("", headers=[("Location", "/asset/items")], status=303)
+        try:
+            params = http.request.get_http_params()
+            http.request.env["aset.barang"].create(
+                {"name": params["name"], "qty": 0})
+            http.request.cr.commit()
+            return http.request.make_response("", headers=[("Location", "/asset/items")], status=303)
+        except Exception as e:
+            return http.request.make_response("", headers=[("Location", f"/asset/items/new?err={e}")], status=303)
 
     @http.route('/asset/items/new', auth='user', website=True, methods=["GET"])
     def items_new(self, **kw):
@@ -38,11 +41,15 @@ class Asset(http.Controller):
 
     @http.route('/asset/items/produce', auth='user', website=True, methods=["POST"])
     def items_produce_handle(self, **kw):
-        params = http.request.get_http_params()
-        record = http.request.env["aset.barang"].browse([int(params["id"])])
-        record.qty += int(params["qty"])
-        http.request.cr.commit()
-        return http.request.make_response("", headers=[("Location", "/asset/items")], status=303)
+        try:
+            params = http.request.get_http_params()
+            record = http.request.env["aset.barang"].browse(
+                [int(params["id"])])
+            record.qty += int(params["qty"])
+            http.request.cr.commit()
+            return http.request.make_response("", headers=[("Location", "/asset/items")], status=303)
+        except Exception as e:
+            return http.request.make_response("", headers=[("Location", f"/asset/items/produce?err={e}")], status=303)
 
     # @http.route('/asset/rentals/', auth='user', website=True)
     # def rentals(self, **kw):
@@ -76,29 +83,35 @@ class Asset(http.Controller):
 
     @http.route('/asset/rentals/new', auth='user', website=True, methods=["POST"])
     def rentals_new_item_handle(self, **kw):
-        params = http.request.get_http_params()
-        rental = http.request.env["aset.penyewaan"].browse(
-            [int(params["rental-id"])])
-        item = http.request.env["aset.barang"].browse(
-            [int(params["item-id"])])
-        http.request.env["aset.barang_sewaan"].create(
-            {"item": item.id, "rental": rental.id, "qty": params["qty"]})
-        http.request.cr.commit()
-        return http.request.make_response("", headers=[("Location", "/asset/rentals")], status=303)
+        try:
+            params = http.request.get_http_params()
+            rental = http.request.env["aset.penyewaan"].browse(
+                [int(params["rental-id"])])
+            item = http.request.env["aset.barang"].browse(
+                [int(params["item-id"])])
+            http.request.env["aset.barang_sewaan"].create(
+                {"item": item.id, "rental": rental.id, "qty": params["qty"]})
+            http.request.cr.commit()
+            return http.request.make_response("", headers=[("Location", "/asset/rentals")], status=303)
+        except Exception as e:
+            return http.request.make_response("", headers=[("Location", f"/asset/rentals/new?err={e}")], status=303)
 
     @http.route('/asset/rentals/new-rental', auth='user', website=True, methods=["POST"])
     def rentals_new_handle(self, **kw):
-        params = http.request.get_http_params()
-        date_component = params["date"].split("/")
-        date_component.reverse()
-        date_component[1], date_component[2] = date_component[2], date_component[1]
-        date = "-".join(date_component)
-        http.request.env["aset.penyewaan"].create(
-            {"client": params["client"],
-             "rental_date": date,
-             "duration": params["duration"]})
-        http.request.cr.commit()
-        return http.request.make_response("", headers=[("Location", "/asset/rentals/new")], status=303)
+        try:
+            params = http.request.get_http_params()
+            date_component = params["date"].split("/")
+            date_component.reverse()
+            date_component[1], date_component[2] = date_component[2], date_component[1]
+            date = "-".join(date_component)
+            http.request.env["aset.penyewaan"].create(
+                {"client": params["client"],
+                 "rental_date": date,
+                 "duration": params["duration"]})
+            http.request.cr.commit()
+            return http.request.make_response("", headers=[("Location", "/asset/rentals/new")], status=303)
+        except Exception as e:
+            return http.request.make_response("", headers=[("Location", f"/asset/rentals/new?err={e}")], status=303)
 
     # @http.route('/asset/returns/<year>/<month>', auth='user', website=True)
     # def returns(self, **kw):
@@ -129,29 +142,35 @@ class Asset(http.Controller):
 
     @http.route('/asset/returns/new', auth='user', website=True, methods=["POST"])
     def returns_new_item_handle(self, **kw):
-        params = http.request.get_http_params()
-        rental = http.request.env["aset.penyewaan"].browse(
-            [int(params["rental-id"])])
-        item = http.request.env["aset.barang"].browse(
-            [int(params["item-id"])])
-        return_ = http.request.env["aset.pengembalian"].browse(
-            [int(params["return-id"])])
-        http.request.env["aset.barang_kembalian"].create(
-            {"item": item.id, "rental": rental.id, "return_": return_.id, "qty": params["qty"]})
-        http.request.cr.commit()
-        return http.request.make_response("", headers=[("Location", "/asset/returns")], status=303)
+        try:
+            params = http.request.get_http_params()
+            rental = http.request.env["aset.penyewaan"].browse(
+                [int(params["rental-id"])])
+            item = http.request.env["aset.barang"].browse(
+                [int(params["item-id"])])
+            return_ = http.request.env["aset.pengembalian"].browse(
+                [int(params["return-id"])])
+            http.request.env["aset.barang_kembalian"].create(
+                {"item": item.id, "rental": rental.id, "return_": return_.id, "qty": params["qty"]})
+            http.request.cr.commit()
+            return http.request.make_response("", headers=[("Location", "/asset/returns")], status=303)
+        except Exception as e:
+            return http.request.make_response("", headers=[("Location", f"/asset/returns/new?err={e}")], status=303)
 
     @http.route('/asset/returns/new-return', auth='user', website=True, methods=["POST"])
     def returns_new_handle(self, **kw):
-        params = http.request.get_http_params()
-        date_component = params["date"].split("/")
-        date_component.reverse()
-        date_component[1], date_component[2] = date_component[2], date_component[1]
-        date = "-".join(date_component)
-        http.request.env["aset.pengembalian"].create(
-            {"return_date": date})
-        http.request.cr.commit()
-        return http.request.make_response("", headers=[("Location", "/asset/returns/new")], status=303)
+        try:
+            params = http.request.get_http_params()
+            date_component = params["date"].split("/")
+            date_component.reverse()
+            date_component[1], date_component[2] = date_component[2], date_component[1]
+            date = "-".join(date_component)
+            http.request.env["aset.pengembalian"].create(
+                {"return_date": date})
+            http.request.cr.commit()
+            return http.request.make_response("", headers=[("Location", "/asset/returns/new")], status=303)
+        except Exception as e:
+            return http.request.make_response("", headers=[("Location", f"/asset/returns/new?err={e}")], status=303)
 
     @http.route('/asset/items/report', auth='user', website=True)
     def items_report(self, **kw):
